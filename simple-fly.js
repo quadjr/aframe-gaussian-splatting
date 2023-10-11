f13546a90a6cf3064feea5af8d87ccbe4ad4599e
Lego-Dimensions <88616437+electrum-bowie@users.noreply.github.com>
2023-10-11 22:36:42 +0300

    s

--- simple-fly.js
+++ simple-fly.js
@@ -1,43 +1,49 @@
-AFRAME.registerComponent(simple-fly, {
-  schema {
-    speed { default 0.1 },
-  },
-  init function () {
-    this.rightController = null;
-    this.isFlying = false;
-    
-     Find and store the right controller.
-    const controllers = this.el.sceneEl.querySelectorAll([hand-controls]);
-    controllers.forEach(controller = {
-      if (controller.getAttribute(hand-controls).hand === right) {
-        this.rightController = controller;
-      }
-    });
-    
-     Add an event listener to start flying when any button is pressed.
-    this.el.addEventListener(abuttondown, this.startFlying.bind(this));
-    this.el.addEventListener(bbuttondown, this.startFlying.bind(this));
-     Add an event listener to stop flying when any button is released.
-    this.el.addEventListener(abuttonup, this.stopFlying.bind(this));
-    this.el.addEventListener(bbuttonup, this.stopFlying.bind(this));
-  },
-  tick function () {
-    if (this.isFlying) {
-      this.fly();
-    }
-  },
-  startFlying function () {
-    this.isFlying = true;
-  },
-  stopFlying function () {
-    this.isFlying = false;
-  },
-  fly function () {
-    if (this.rightController) {
-      const direction = new THREE.Vector3();
-      this.rightController.object3D.getWorldDirection(direction);
-      direction.multiplyScalar(this.data.speed);
-      this.el.object3D.position.add(direction);
-    }
-  }
+AFRAME.registerComponent('simple-fly', {
+    schema: {
+        speed: { type: 'number', default: 0.1 },
+    },
+    init: function () {
+        this.isFlying = false;
+        this.rightController = null;
+
+        // Register a reference to the right Oculus Touch controller.
+        this.rightController = document.querySelector('[oculus-touch-controls]:not([hand="left"])');
+
+        if (!this.rightController) {
+            console.error('No right Oculus Touch controller found.');
+            return;
+        }
+
+        // Listen to the "gripdown" and "gripup" events to start and stop flying.
+        this.rightController.addEventListener('gripdown', () => {
+            this.startFlying();
+        });
+
+        this.rightController.addEventListener('gripup', () => {
+            this.stopFlying();
+        });
+    },
+    tick: function () {
+        if (this.isFlying) {
+            this.fly();
+        }
+    },
+    startFlying: function () {
+        this.isFlying = true;
+    },
+    stopFlying: function () {
+        this.isFlying = false;
+    },
+    fly: function () {
+        if (this.rightController) {
+            const direction = new THREE.Vector3();
+            this.rightController.object3D.getWorldDirection(direction);
+
+            // Normalize the direction and multiply by the speed.
+            direction.normalize().multiplyScalar(this.data.speed);
+
+            // Apply the direction to the entity's position.
+            this.el.object3D.position.add(direction);
+        }
+    },
 });
